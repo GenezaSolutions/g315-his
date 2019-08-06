5400597218b319111a229226dccb97a6c35dc3ce
Binu Alexander <binu.alexander@icloud.com>
2019-08-06 16:59:28 +0530

    more on Patients

--- application/core/models.py
+++ application/core/models.py
@@ -3,6 +3,12 @@ from django.db import models
 # Create your models here.
 
 class  Patient(models.Model):
-	first_name = models.CharField(max_length=200)
-	last_name = models.CharField(max_length=200)
-	
+	first_name = models.CharField(max_length=80)
+	last_name = models.CharField(max_length=80)
+	age_firstentry = models.IntegerField(default = 0)
+	gender = [
+    ('M', 'Male'),
+    ('F', 'Female'),
+    ('US', 'Unspecified'),
+    
+		]
