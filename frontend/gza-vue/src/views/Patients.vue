84849454b8a55884ccf4f54539e5210555bbf1de
Binu Alexander <binu.alexander@icloud.com>
2019-08-22 15:20:35 +0200

    Update Patients.vue

--- frontend/gza-vue/src/views/Patients.vue
+++ frontend/gza-vue/src/views/Patients.vue
@@ -2,12 +2,15 @@
   <div id="app" class="container">
     <p v-if="loading">Loading...</p>
     <div v-else>
-      <h3 class="heading">Patients</h3>
+      <h3 class="heading" style="text-align:left">Patients</h3>
+      <input  v-model = "searchstring" placeholder ="Search here">
       <table class="table table-bordered">
         <thead>
           <tr>
             <th scope="col">ID</th>
+            <th scope="col">Name</th>
             <th scope="col">Email</th>
+           
           </tr>
         </thead>
         <tbody>
@@ -36,7 +39,7 @@ export default {
   mounted () {
     this.loading = true;
     axios
-      .get('http://localhost:9000/Patients/?format=json')
+      .get('http://localhost:8000/Patients/?format=json')
       .then(response => (this.all_patients = response.data))
       .catch(error => console.log(error))
       .finally(() => this.loading = false)
