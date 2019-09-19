5af29dfb84e4d2907a59f8c6406d95c414a22d35
Binu Alexander <binu.alexander@icloud.com>
2019-09-19 18:57:08 +0530

    Dynamic Routing fixed

--- frontend/gza-vue/src/views/PatientDetail.vue
+++ frontend/gza-vue/src/views/PatientDetail.vue
@@ -1,98 +1,66 @@
 <template>
   <div id="PatientList" class="container">
-    
     <div>
       <h3 class="heading" style="text-align:left">ID {{$route.params.id}}</h3>
-      <input id="lens" v-model= "search" placeholder ="Search here">
-      <br><br>
-
-
-      
-  </div>
-
-
-      <table class="table table-bordered">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Name</th>
-            <th scope="col">Mobile</th>
-            <th scope="col">Email</th>
-            
-          
-           
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-bind:key="patient">
-            <td>{{ patient.id }}</td>
-            <td>{{ patient.first_name + " " + patient.last_name }}</td>
-            <td>{{ patient.mobile }}</td>
-            <td>{{ patient.email }}</td>
-            <td>></td>
-          </tr>
-        </tbody>
-      </table>
-
-
-
-   
+      <br />
+      <br />
     </div>
 
-   
-  
+    <table class="table table-bordered">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Name</th>
+          <th scope="col">Mobile</th>
+          <th scope="col">Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-bind:key="patient">
+          <td>{{ patient.id }}</td>
+          <td>{{ patient.first_name + " " + patient.last_name }}</td>
+          <td>{{ patient.mobile }}</td>
+          <td>{{ patient.email }}</td>
+          <td>></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 export default {
-  name: 'PatientList',
-  data () {
+  name: "PatientList",
+  data() {
     return {
-      
-      patient: '',
-      
-      
-      
-      
-    }
+      patient: ""
+    };
   },
-  created () {
-    
+  created() {
     this.fetchData();
-    
-      
-  },
- 
- watch: {
-  '$route': 'fetchData'
-
-  
   },
 
+  watch: {
+    $route: "fetchData"
+  },
 
-methods: { fetchData () {
-  axios
-      .get('http://localhost:8000/Patients/'+ this.$route.params.id +'/?format=json')
-      .then(response => (this.patient = Object.assign({}, this.patient, response.data)))
-      .catch(error => console.log(error))
-     }
-
-  
-
-
-}
-
-
-
-
-}
-
-
- 
- 
-
-
+  methods: {
+    fetchData() {
+      axios
+        .get(
+          "http://localhost:8000/Patients/" +
+            this.$route.params.id +
+            "/?format=json"
+        )
+        .then(
+          response =>
+            (this.patient = Object.assign({}, this.patient, response.data))
+        )
+        .catch(error => console.log(error));
+    }
+  }
+};
 </script>
 
 <style>
@@ -105,9 +73,7 @@ methods: { fetchData () {
 }
 
 #lens {
-
-position: relative;
-left: -468px;
+  position: relative;
+  left: -468px;
 }
- 
 </style>
